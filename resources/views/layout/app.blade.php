a6ce38c8a5d53b73ad507480215c2835cc76bb85
Choung Chanphorsda <choung.chanphorsda.2822@rupp.edu.kh>
2025-06-18 09:47:04 +0700

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,7 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        @vite('resources/css/app.css')
+       @vite(['resources/css/app.css', 'resources/js/app.js'])
         
         <!-- Font Awesome for icons -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
